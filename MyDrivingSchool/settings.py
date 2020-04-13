82ca51a97d301ee2ae7c7a69e47cdd1ed5317c23
MrKafe <alexandre.rosselange@epitech.eu>
2020-04-13 20:08:59 +0200

    USER::Add force login middleware

--- MyDrivingSchool/settings.py
+++ MyDrivingSchool/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'User.middleware.auth_required_middleware'
 ]
 
 ROOT_URLCONF = 'MyDrivingSchool.urls'
---
+++ User/middleware.py
@@ -0,0 +1,12 @@
+from django.shortcuts import redirect, reverse
+
+
+def auth_required_middleware(get_response):
+    def process_request(request):
+        if (not request.user.is_authenticated and
+                not request.path.startswith(reverse('login'))):
+            print("\033[1;91mAUTHENTICATION REQUESTED\033[m")
+            return redirect('login')
+        return get_response(request)
+
+    return process_request
