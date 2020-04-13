3ad276fea5bf4aba8fbf67f0767f946d24f671a0
MrKafe <alexandre.rosselange@epitech.eu>
2020-04-13 16:46:45 +0200

    USER::Add login

--- User/urls.py
+++ User/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login', views.user_login, name='login'),
     path('show/<int:user_id>', views.show, name='show_user'),
 ]
--- User/views.py
+++ User/views.py
@@ -1,5 +1,31 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from . import forms
+
+
+def user_login(request):
+    error = False
+
+    if request.method == "POST":
+        print('\033[96m> Trying to log in\033[m')
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user:
+                print('\033[92m> Logging in user ['+username+']\033[m')
+                login(request, user)
+                return redirect('dashboard')
+            else:
+                print('\033[91m> Could not find user ['+username+']\033[m')
+                error = True
+    else:
+        print('\033[96m> Accessed login page\033[m')
+        form = forms.LoginForm()
+
+    return render(request, 'User/login.html', locals())
 
 
 def show(request, user_id=None):
