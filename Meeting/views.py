ca3ee9e174b303ff9bd0e68e9eecae229c5223b4
MrKafe <alexandre.rosselange@epitech.eu>
2020-04-17 16:05:57 +0200

    MEETING::Update verbose and permissions

--- Meeting/views.py
+++ Meeting/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.exceptions import PermissionDenied
 
 from . import forms
-from User.utilities import is_granted, has_group
+from User.utilities import is_granted, has_group, get_highest_role
 from Meeting.utilities import get_datetime_from_date_hour_minute as get_date
 from Meeting.models import Meeting
 from django.contrib.auth.models import User
@@ -13,7 +13,7 @@ from User.models import Profile
 
 
 def index(request, filter=None):
-    print('\033[96m> Accessing appointment page\033[m')
+    print('\033[96m> Access to appointment list\033[m')
 
     if is_granted(request.user, 'secretary'):
         if filter:
@@ -24,10 +24,13 @@ def index(request, filter=None):
             elif has_group(user, 'instructor'):
                 appointments = Meeting.objects.filter(instructor=user)
             else:
-                return HttpResponseServerError("Impossible to solve this selection, stop playing with the URL, damn human")
+                return HttpResponseServerError('Impossible to solve this selection,'\
+                   ' stop playing with the URL, damn human')
         else:
             appointments = Meeting.objects.all()
             view_name = '_admin_row.html'
+        print('\033[96m - Filters : '+(('On: '+'['+user.username+' ('+get_highest_role(user)+')]') if filter
+           else 'Off')+'\033[m')
     elif is_granted(request.user, 'instructor'):
         appointments = Meeting.objects.filter(instructor=request.user)
         view_name = '_instructor_row.html'
@@ -35,7 +38,7 @@ def index(request, filter=None):
         appointments = Meeting.objects.filter(student=request.user)
         view_name = '_student_row.html'
     else:
-        raise HttpResponseServerError
+        return HttpResponseServerError('This is unexpected... ><')
 
     print('\033[96m - View : '+view_name+'\033[m')
 
@@ -44,14 +47,13 @@ def index(request, filter=None):
 
 def create(request):
     if not is_granted(request.user, 'instructor'):
+        print('\033[1;91m< User have to be granted [instructor] to create appointments\033[m')
         raise PermissionDenied
 
     if has_group(request.user, 'instructor'):
         add_instructor = False
     else:
         add_instructor = True
-
-    print('\033[96m> Accessing create appointment page\033[m')
 
     has_error = False
     if request.method == "POST":
@@ -76,12 +78,12 @@ def create(request):
             if end_at <= start_at:
                 has_error = True
                 error = 'The appointment have to end AFTER it begin'
+                print('\033[91m✕ Could not create appointment: bad hours value \033[m')
             elif t_remains_delta < duration_delta:
                 has_error = True
                 error = str(student)+' does not have enough hours: '+str(student.profile.time)+'h remaining'
+                print('\033[91m✕ Could not create appointment: not enough student credits \033[m')
             else:
-                print('\033[96m> Creating new appoint for student/instructor '+str(student)+'/'+str(instructor)+' : '+\
-                      strftime('%H:%M', gmtime(duration_delta.total_seconds()))+'h\033[m')
                 meeting = Meeting(
                     student=student,
                     instructor=instructor,
@@ -94,77 +96,83 @@ def create(request):
                 student.profile.time = strftime('%H:%M', gmtime((t_remains_delta - duration_delta).total_seconds()))
                 meeting.save()
                 student.profile.save()
+
+                print('\033[96m> Created new appointment for student/instructor:\n'
+                    '   ['+str(student)+'/'+str(instructor)+'] => '+str(meeting.hours)+ 'h\033[m')
+
                 return redirect('index_meet')
         else:
             has_error = True
             error = 'An error has occurred'
+            print('\033[91m✕ Invalid form\033[m')
     else:
-        print('\033[96m> Accessed create meeting page\033[m')
+        print('\033[96m> Access to create appointment\n - Instructor choice: '+('On' if add_instructor else 'Off')
+            +'\033[m')
         form = forms.CreateForm(add_instructor=add_instructor)
 
     return render(request, 'Meeting/create.html', locals())
 
 
 def edit(request, meet_id):
-    if not is_granted(request.user, 'instructor'):
-        raise PermissionDenied
-
     try:
         meet = Meeting.objects.get(pk=meet_id)
     except Meeting.DoesNotExist:
-        raise Http404("No MyModel matches the given query.")
+        print('\033[1;91m< Cound not find appointment ['+str(meet_id)+']\033[m')
+        raise Http404
 
-    if has_group(request.user, 'instructor'):
-        add_instructor = False
-    else:
-        add_instructor = True
-        print("ADD INSTRUCTORS")
-
-    if request.method == "POST":
-        print('\033[96m> Trying to create meeting\033[m')
-
-        form = forms.EditForm(request.POST, add_instructor=add_instructor)
-        if form.is_valid():
-            student = form.cleaned_data['student']
-            if add_instructor:
-                instructor = form.cleaned_data['instructor']
-            else:
-                instructor = request.user
-            date = form.cleaned_data['date']
-            start_at = timedelta(hours=int(form.cleaned_data['start_at_h']), minutes=int(form.cleaned_data['start_at_m']))
-            end_at = timedelta(hours=int(form.cleaned_data['end_at_h']), minutes=int(form.cleaned_data['end_at_m']))
-            location = form.cleaned_data['location']
-
-            duration_delta = end_at - start_at
-            duration_delta_string = strftime('%H:%M', gmtime(duration_delta.total_seconds()))
-
-            old_start_at = timedelta(hours=meet.start_at.hour, minutes=meet.start_at.minute)
-            old_end_at = timedelta(hours=meet.end_at.hour, minutes=meet.end_at.minute)
-            diff = gmtime((old_end_at - old_start_at).total_seconds())
-            old_duration_delta = timedelta(hours=diff.tm_hour, minutes=diff.tm_min)
-            t_remains_delta = timedelta(hours=meet.student.profile.time.hour, minutes=meet.student.profile.time.minute) + old_duration_delta
-
-            if end_at <= start_at:
-                has_error = True
-                error = 'The appointment have to end AFTER it begin'
-            elif t_remains_delta < duration_delta:
-                has_error = True
-                error = str(meet.student) + ' does not have enough hours: ' + meet.student.profile.time.strftime('%H:%M') + 'h remaining'
-            else:
-                print('\033[96m> Updating appointment for student/instructor ' + str(meet.student) + '/' +\
-                      str(instructor)+'\033[m')
-                meet.date = date
-                meet.start_at = strftime('%H:%M', gmtime(start_at.total_seconds()))
-                meet.end_at = strftime('%H:%M', gmtime(end_at.total_seconds()))
-                meet.hours = strftime('%H:%M', gmtime(duration_delta.total_seconds()))
-                meet.location = location
-                meet.save()
-                meet.student.profile.time = strftime('%H:%M', gmtime((t_remains_delta - duration_delta).total_seconds()))
-                meet.student.profile.save()
-                return redirect('index_meet')
-
-    if (is_granted(request.user, 'instructor') and meet.instructor.id is request.user.id)\
+    if (is_granted(request.user, 'instructor') and meet.instructor.id is request.user.id) \
             or (is_granted(request.user, 'secretary')):
+
+        if has_group(request.user, 'instructor'):
+            add_instructor = False
+        else:
+            add_instructor = True
+
+        if request.method == "POST":
+            print('\033[96m> Trying to edit meeting\033[m')
+            form = forms.EditForm(request.POST, add_instructor=add_instructor)
+            if form.is_valid():
+                date = form.cleaned_data['date']
+                start_at = timedelta(hours=int(form.cleaned_data['start_at_h']), minutes=int(form.cleaned_data['start_at_m']))
+                end_at = timedelta(hours=int(form.cleaned_data['end_at_h']), minutes=int(form.cleaned_data['end_at_m']))
+                location = form.cleaned_data['location']
+
+                duration_delta = end_at - start_at
+                duration_delta_string = strftime('%H:%M', gmtime(duration_delta.total_seconds()))
+
+                old_start_at = timedelta(hours=meet.start_at.hour, minutes=meet.start_at.minute)
+                old_end_at = timedelta(hours=meet.end_at.hour, minutes=meet.end_at.minute)
+                diff = gmtime((old_end_at - old_start_at).total_seconds())
+                old_duration_delta = timedelta(hours=diff.tm_hour, minutes=diff.tm_min)
+                t_remains_delta = timedelta(hours=meet.student.profile.time.hour, minutes=meet.student.profile.time.minute) + old_duration_delta
+
+                if end_at <= start_at:
+                    has_error = True
+                    error = 'The appointment have to end AFTER it begin'
+                    print('\033[91m✕ Could not edit appointment: bad hours value \033[m')
+                elif t_remains_delta < duration_delta:
+                    has_error = True
+                    error = str(meet.student)+' does not have enough hours: '+meet.student.profile.time.strftime('%H:%M')+\
+                        'h remaining'
+                    print('\033[91m✕ Could not edit appointment: not enough student credits \033[m')
+                else:
+                    print('\033[96m> Updating appointment for student/instructor ' + str(meet.student) + '/' +\
+                          str(meet.instructor)+'\033[m')
+                    meet.date = date
+                    meet.start_at = strftime('%H:%M', gmtime(start_at.total_seconds()))
+                    meet.end_at = strftime('%H:%M', gmtime(end_at.total_seconds()))
+                    meet.hours = strftime('%H:%M', gmtime(duration_delta.total_seconds()))
+                    meet.location = location
+                    meet.save()
+
+                    meet.student.profile.time = strftime('%H:%M', gmtime((t_remains_delta - duration_delta).total_seconds()))
+                    meet.student.profile.save()
+
+                    print('\033[96m> Created new appointment for student/instructor:\n'
+                        '   ['+str(meet.student)+'/'+str(meet.instructor)+'] => '+str(meet.hours)+'h\033[m')
+
+                    return redirect('index_meet')
+
         data = {
             'date': meet.date,
             'start_at_h': meet.start_at.strftime("%H"),
@@ -182,13 +190,18 @@ def edit(request, meet_id):
         data['start_at_m'] = '{}'.format(data['start_at_m'][1:] if data['start_at_m'].startswith('0') else data['start_at_m'])
         data['end_at_h'] = '{}'.format(data['end_at_h'][1:] if data['end_at_h'].startswith('0') else data['end_at_h'])
         data['end_at_m'] = '{}'.format(data['end_at_m'][1:] if data['end_at_m'].startswith('0') else data['end_at_m'])
-        # form = forms.CreateForm(data, add_instructor=add_instructor)
+
         form = forms.EditForm(data=data, initial=initial, add_instructor=add_instructor)
+
+        print('\033[96m> Access to edit appointment\n - Instructor choice: ' + ('On' if add_instructor else 'Off')
+              + '\033[m')
 
         action_param = {'id': meet_id}
         is_edit = True
         return render(request, 'Meeting/create.html', locals())
     else:
+        print('\033[1;91m< User have to be\n - granted [instructor] and own appointment\n'\
+            ' - granted secretary\033[m')
         raise PermissionDenied
 
 
@@ -196,7 +209,10 @@ def delete(request, meet_id):
     try:
         meet = Meeting.objects.get(pk=meet_id)
     except Meeting.DoesNotExist:
+        print('\033[1;91m< Cound not find appointment [' + str(meet_id) + ']\033[m')
         raise Http404()
+
+    print('\033[96m> Trying to delete meeting\033[m')
 
     if (is_granted(request.user, 'instructor') and meet.instructor.id is request.user.id) \
             or (is_granted(request.user, 'secretary')):
@@ -209,6 +225,8 @@ def delete(request, meet_id):
         meet.student.profile.save()
         meet.delete()
     else:
+        print('\033[1;91m< User have to be\n - granted [instructor] and own appointment\n'\
+            ' - granted secretary\033[m')
         raise PermissionDenied
 
     return redirect('index_meet')
